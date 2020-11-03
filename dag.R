ef30d5ad44811b307a05be4f90031a615a42d9c9
MeganMarziali <mem2371@cumc.columbia.edu>
2020-11-02 17:35:46 -0800

    updated DAG

--- dag.R
+++ dag.R
@@ -1,15 +1,17 @@
 library(ggdag)
 theme_set(theme_dag())
 
-suidicality.dag <- dagify(suicidality ~ PO_use + NO_social_support + demographics + other_substance_use + mental_health, 
-                          PO_use ~ demographics + other_substance_use + mental_health, 
+suidicality.dag <- dagify(suicidality ~ PO_use + NO_social_support + demographics + gender + other_substance_use + mental_health, 
+                          PO_use ~ demographics + gender + other_substance_use + mental_health, 
                           mental_health_treatment ~ mental_health, 
+                          NO_social_support ~ gender + other_substance_use + mental_health,
                           latent = "mental_health", 
                           exposure = c("PO_use", "NO_social_support"), 
                           outcome = "suicidality", 
                           labels = c("suicidality" = "Suicidality", 
                                      "PO_use" = "PO Use", 
-                                     "demographics" = "Female,\n Race,\n Temporal Trends,\n County Type", 
+                                     "gender" = "Gender",
+                                     "demographics" = "Race,\n Temporal Trends,\n County Type", 
                                      "other_substance_use" = "Other Substance Use", 
                                      "mental_health" = "Mental Disorders", 
                                      "NO_social_support" = "No Social Support", 
